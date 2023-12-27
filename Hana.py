99952e336bad4de13f3c2c95fe0d9af56f4a0f64
Kelniter <154971168+Kelniter@users.noreply.github.com>
2023-12-28 01:44:25 +0700

    Hii !

---
+++ Hana.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras import layers
+
+class ModelOne(Model):
+  def __init__(self, input_shape):
+    self.null = null
+
+class HanaModel(Model):
+  def __init__(self, final_node):
+    self.null = null
